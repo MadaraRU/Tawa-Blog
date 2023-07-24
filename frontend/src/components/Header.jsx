3de764055528934f6fcece306f09ccca0253a733
MadaraRU <muhammedkhalil@mail.ru>
2023-07-24 14:26:16 +0100

    Frontend setup

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,18 @@
+import { Container, Nav, Navbar } from 'react-bootstrap';
+
+const Header = () => {
+  return (
+    <header>
+      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+        <Container>
+          <Navbar.Brand>TAWA</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto"></Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+};
+export default Header;
